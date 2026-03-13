bce12d980c8ac62b66500b4b10a22ab97f9fadc1
ardashthirteen <arDashThirteen@gmail.com>
2026-03-13 08:14:33 +0800

    add js

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@ let secondCard = 11;
 
 let sum = firstCard + secondCard;
 
-if (sum < 21) {
-    console.log("do you want to draw a new card?");
-} else if (sum === 21) {
-    console.log("wohoo! you've got blackjack!");
-} else if (sum > 21) {
-    console.log("you're out of the game.");
+function cliked() {
+    if (sum < 21) {
+        alert("do you want to draw a new card?");
+    } else if (sum === 21) {
+        alert("wohoo! you've got blackjack!");
+    } else if (sum > 21) {
+        alert("you're out of the game.");
+    }
 }
