276ddd83802abada54f2ffb5f2829179bbf3fc2c
ardashthirteen <arDashThirteen@gmail.com>
2026-03-13 08:18:42 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,13 +4,12 @@ let secondCard = 11;
 let sum = firstCard + secondCard;
 
 function cliked() {
-    if (sum < 21) {
-        alert("do you want to draw a new card?");
-    } else if (sum === 21) {
-        alert("wohoo! you've got blackjack!");
-    } else if (sum > 21) {
-        alert("you're out of the game.");
-    }
+    // if (sum < 21) {
+    //     alert("do you want to draw a new card?");
+    // } else if (sum === 21) {
+    //     alert("wohoo! you've got blackjack!");
+    // } else if (sum > 21) {
+    //     alert("you're out of the game.");
+	// }
+	alert(sum)
 }
-
-alert(sum)
